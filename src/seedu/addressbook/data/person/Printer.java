57d4bedc62b533e27f1f050f1c8353e65879b496
Hui Qi <xxstarsgalorexx@gmail.com>
2016-09-06 15:58:54 +0800

    Use Interfaces to apply ISP

---
+++ src/seedu/addressbook/data/person/Printer.java
@@ -0,0 +1,13 @@
+package seedu.addressbook.data.person;
+ 
+import java.util.StringJoiner;
+ 
+public class Printer {  
+    public static String getPrintableString(Printable...printables) {
+        StringJoiner joiner = new StringJoiner(", ");
+        for (Printable p : printables) {
+            joiner.add(p.getPrintableString());
+        }
+        return joiner.toString();
+    } 
+}
